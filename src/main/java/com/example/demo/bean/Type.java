10b2a8673cb6c325664e62a082b2faa959a03664
Jasmine QIAN <jasqia@acxiom.com>
2019-06-18 22:41:44 +0800

    type add creationdt

--- src/main/java/com/example/demo/bean/Type.java
+++ src/main/java/com/example/demo/bean/Type.java
@@ -4,6 +4,10 @@ public class Type {
 
     private int id;
     private String name;
+    private String creationdt;
+    private String updatedt;
+    private String creationuid;
+    private String updateuid;
 
     public int getId() {
         return id;
@@ -21,11 +25,47 @@ public class Type {
         this.name = name;
     }
 
+    public String getCreationdt() {
+        return creationdt;
+    }
+
+    public void setCreationdt(String creationdt) {
+        this.creationdt = creationdt;
+    }
+
+    public String getUpdatedt() {
+        return updatedt;
+    }
+
+    public void setUpdatedt(String updatedt) {
+        this.updatedt = updatedt;
+    }
+
+    public String getCreationuid() {
+        return creationuid;
+    }
+
+    public void setCreationuid(String creationuid) {
+        this.creationuid = creationuid;
+    }
+
+    public String getUpdateuid() {
+        return updateuid;
+    }
+
+    public void setUpdateuid(String updateuid) {
+        this.updateuid = updateuid;
+    }
+
     @Override
     public String toString() {
         return "Type{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", creationdt='" + creationdt + '\'' +
+                ", updatedt='" + updatedt + '\'' +
+                ", creationuid='" + creationuid + '\'' +
+                ", updateuid='" + updateuid + '\'' +
                 '}';
     }
 }
